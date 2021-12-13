14dad3daa386db960d16a6638ced00bdead19a4a
David DeKime <dcdekime@optonline.net>
2021-12-12 23:55:55 -0500

    Initial Commit

---
+++ CTCI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CTCI
+//
+//  Created by David DeKime on 12/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
